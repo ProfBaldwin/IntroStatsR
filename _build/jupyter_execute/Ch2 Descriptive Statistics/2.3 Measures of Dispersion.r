e5a13b572699bc1372845f7b07948a3d33e14068
ProfBaldwin <57511856+ProfBaldwin@users.noreply.github.com>
2021-11-29 15:45:47 -0800

    Continuing to Update

--- _build/jupyter_execute/Ch2 Descriptive Statistics/2.3 Measures of Dispersion.r
+++ _build/jupyter_execute/Ch2 Descriptive Statistics/2.3 Measures of Dispersion.r
@@ -155,3 +155,19 @@ if (length(outliers_below) > 0) {
 }
 
 dev.off()
+
+x = c(1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 10, 11, 11, 13, 13, 14, 15)
+
+# Calculate the Mean
+n = length(x)
+
+xbar = sum(x)/n
+xbar
+
+s = sqrt( sum( (x - xbar)^2 )/(n - 1) )
+s
+
+xbar + 2*s
+xbar - 2*s
+
+## To Do: Video Example Outliers
