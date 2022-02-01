932c9b37a21566a8ad2f3b16a111e08a54f6ab09
ProfBaldwin <57511856+ProfBaldwin@users.noreply.github.com>
2022-01-31 17:05:39 -0800

    3.2 Videos

--- _build/jupyter_execute/Ch3 Probability/3.2 Conditional Probability.r
+++
@@ -1,5 +0,0 @@
-## To Do: Video Example AND and OR, maybe with a table
-
-## To Do: Video Example Conditional Probability Equally Likely
-
-## To Do: Example Video Conditional Probability Not Equally Likely
